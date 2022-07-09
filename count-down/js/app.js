901a9ffd8861ab13c5ee71788f8fe464875a8c91
bhagirath-wscube <bhagirath.wscube@gmail.com>
2022-07-09 10:57:23 +0530

    Latest Of 09-07-2022

--- count-down/js/app.js
+++ count-down/js/app.js
@@ -1,4 +1,4 @@
-const endDate = "27 May 2022 08:20:00 PM"
+const endDate = "27 July 2022 08:20:00 PM"
 
 document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input")
