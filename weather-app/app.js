f182abf275a6f8b37a6ca7868b5c42abd6e9e469
Shaswata Saha <113383437+subhro1530@users.noreply.github.com>
2022-12-23 02:17:46 +0530

    Update app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,6 +2,7 @@ const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
 const form = document.querySelector("form")
 const search = document.querySelector("#search")
 const weather = document.querySelector("#weather")
+
     // const API = `https://api.openweathermap.org/data/2.5/weather?
     // q=${city}&appid=${API_KEY}&units=metric`
     // const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
